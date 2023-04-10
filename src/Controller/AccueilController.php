20681496f7288682bf644d6754435f5c776c6378
Berthouly <jbjb19530@gmail.com>
2023-04-10 17:53:43 +0200

    Translation

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,14 +2,13 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
-    #[Route('/accueil', name: 'app_accueil', methods: ['GET'])]
+    #[Route('/{_locale}/accueil', name: 'app_accueil',requirements: ['_loacle' => 'fr|en'], methods: ['GET'])]
     public function index(): Response
     {
         $user = $this->getUser(); // Obtenez l'utilisateur courant
@@ -17,7 +16,6 @@ class AccueilController extends AbstractController
 
         return $this->render('accueil/index.html.twig', [
             'user' => $user,
-            'controller_name' => 'AccueilController',
         ]);
     }
 }
