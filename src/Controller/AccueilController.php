584085a2e1eef0965fc3749ab857046d53211d79
Berthouly <jbjb19530@gmail.com>
2023-04-11 13:52:48 +0200

    Translation

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,18 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
     #[Route('/{_locale}/accueil', name: 'app_accueil',requirements: ['_loacle' => 'fr|en'], methods: ['GET'])]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $user = $this->getUser(); // Obtenez l'utilisateur courant
+        $locale = $request->getLocale();
+        $request->setLocale('fr');
 
 
         return $this->render('accueil/index.html.twig', [
+            'locale' => $locale,
             'user' => $user,
         ]);
     }
