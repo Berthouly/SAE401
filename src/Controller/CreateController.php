e7bf75c21c6f7173316d615b9e53390e1090157d
Taiyousan <shamsbenhamou@gmail.com>
2023-04-17 16:55:06 +0200

    ovh

--- src/Controller/CreateController.php
+++ src/Controller/CreateController.php
@@ -128,12 +128,12 @@ class CreateController extends AbstractController
                 
 
                 if($locale == 'fr') {
-                    $url = "http://mmi21g01.sae401/jeu/partiefr/$partieID/" . base64_encode($joueur); // ligne
+                    $url = "http://mmi21g01.sae401.ovh/jeu/partiefr/$partieID/" . base64_encode($joueur); // ligne
                     // $url = "http://127.0.0.1:5173/partiefr/$partieID/" . base64_encode($joueur); local
 
                 } else {
                     // $url = "http://127.0.0.1:5173/partieen/$partieID/" . base64_encode($joueur); local
-                    $url = "http://mmi21g01.sae401/jeu/partieen/$partieID/" . base64_encode($joueur); // ligne
+                    $url = "http://mmi21g01.sae401.ovh/jeu/partieen/$partieID/" . base64_encode($joueur); // ligne
 
                 }
 
